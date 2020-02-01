edf375c3cc120c65f21b448388d5559613e5cf8e
CassioBR <cassiocaninde@hotmail.com>
2020-02-01 19:01:49 -0300

    first commit

---
+++ IPNGapp/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDE5Mklnfbe1N8rA7vW8g_5zlazJxusMRw',
+    authDomain: 'ipng-app.firebaseapp.com',
+    databaseURL: 'https://ipng-app.firebaseio.com',
+    projectId: 'ipng-app',
+    storageBucket: 'ipng-app.appspot.com',
+    messagingSenderId: '274933828245',
+    appId: '1:274933828245:web:045407d06af8e723d5699c',
+    measurementId: 'G-0FMT809684'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
